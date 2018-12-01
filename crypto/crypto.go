d76227f7242b31545ba6f7b3959ab1c65e523428
Mitsuko Megumi <mitsukomegumii@gmail.com>
2018-12-01 17:06:09 -0500

    Init sha3.

--- crypto/crypto.go
+++ crypto/crypto.go
@@ -1,1 +1,14 @@
 package crypto
+
+import (
+	"golang.org/x/crypto/sha3"
+)
+
+// Sha3 - hash specified byte array
+func Sha3(b []byte) []byte {
+	hash := sha3.New256() // Init hasher
+
+	hash.Write(b) // Write
+
+	return hash.Sum(nil) // Return final hash
+}
--- crypto/crypto_test.go
+++ crypto/crypto_test.go
@@ -1,1 +1,15 @@
 package crypto
+
+import "testing"
+
+// TestSha3 - test functionality of sha3 hashing function
+func TestSha3(t *testing.T) {
+	hashed := Sha3([]byte("test")) // Hash
+
+	if hashed != nil { // Check is not nil
+		t.Errorf("invalid hash %s", hashed) // Log found error
+		t.FailNow()                         // Panic
+	}
+
+	t.Log(hashed) // Log hashed
+}
