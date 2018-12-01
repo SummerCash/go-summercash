64d2900b51d8ca50d49831e8f9a567728d18993c
Mitsuko Megumi <mitsukomegumii@gmail.com>
2018-12-01 18:35:13 -0500

    Added commonhex definitions testing.

--- common/commonhex_test.go
+++ common/commonhex_test.go
@@ -1,1 +1,37 @@
 package common
+
+import "testing"
+
+// TestMemPrefix - test MemPrefix definition
+func TestMemPrefix(t *testing.T) {
+	if string(MemPrefix) != "0x" || len(MemPrefix) != 2 { // Check invalid memPrefix
+		t.Errorf("invalid memory prefix %s", string(MemPrefix)) // Log found error
+		t.FailNow()                                             // Panic
+	}
+
+	t.Log(string(MemPrefix)) // Log success
+}
+
+// TestErrNilInput - test ErrNilInput error definition
+func TestErrNilInput(t *testing.T) {
+	_, err := EncodeString([]byte{}) // Encode string
+
+	if err != nil && err != ErrNilInput { // Check for errors
+		t.Error(err) // Log found error
+		t.FailNow()  // Panic
+	}
+
+	t.Logf("success: %s", err.Error()) // Log success
+}
+
+// TestErrNoMem - test ErrNoMem error definition
+func TestErrNoMem(t *testing.T) {
+	_, err := EncodeString([]byte("0")) // Encode string
+
+	if err != nil && err != ErrNoMem { // Check for errors
+		t.Error(err) // Log found error
+		t.FailNow()  // Panic
+	}
+
+	t.Logf("success: %s", err.Error()) // Log success
+}
