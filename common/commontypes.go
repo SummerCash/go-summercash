96d7e97b295d77a6f92f526fb3a5e39d804f8555
Mitsuko Megumi <mitsukomegumii@gmail.com>
2018-12-01 16:05:58 -0500

    Unit testing for all address conversion methods.

--- common/commontypes.go
+++ common/commontypes.go
@@ -23,6 +23,22 @@ func NewAddress(privateKey *ecdsa.PrivateKey) (Address, error) {
 	return marhsaledPublicKey[0:32], nil // Return public key
 }
 
+// StringToAddress - convert string to address
+func StringToAddress(s string) (Address, error) {
+	decoded, err := hex.DecodeString(s) // Decode string
+
+	if err != nil { // Check for errors
+		return Address{}, err // Return found error
+	}
+
+	return decoded, nil // Return address
+}
+
+// Bytes - convert given address to bytes
+func (address Address) Bytes() []byte {
+	return address // Return byte val
+}
+
 // String - convert given address to string
 func (address Address) String() string {
 	return hex.EncodeToString([]byte(address[:])) // Return string val
--- common/commontypes_test.go
+++ common/commontypes_test.go
@@ -7,6 +7,78 @@ import (
 	"testing"
 )
 
+/* BEGIN EXPORTED METHODS */
+
+// TestNewAddress - test init new address
+func TestNewAddress(t *testing.T) {
+	privateKey, err := ecdsa.GenerateKey(elliptic.P521(), rand.Reader)
+
+	if err != nil { // Check for errors
+		t.Error(err) // Log found error
+		t.FailNow()  // Panic
+	}
+
+	address, err := NewAddress(privateKey) // Generate address
+
+	if err != nil { // Check for errors
+		t.Error(err) // Log found error
+		t.FailNow()  // Panic
+	}
+
+	t.Logf("0x%s", address) // Log address
+}
+
+func TestStringToAddress(t *testing.T) {
+	privateKey, err := ecdsa.GenerateKey(elliptic.P521(), rand.Reader)
+
+	if err != nil { // Check for errors
+		t.Error(err) // Log found error
+		t.FailNow()  // Panic
+	}
+
+	address, err := NewAddress(privateKey) // Generate address
+
+	if err != nil { // Check for errors
+		t.Error(err) // Log found error
+		t.FailNow()  // Panic
+	}
+
+	stringVal := address.String() // Get byte val
+
+	if err != nil { // Check for errors
+		t.Error(err) // Log found error
+		t.FailNow()  // Panic
+	}
+
+	decoded, err := StringToAddress(stringVal) // Decode string
+
+	if err != nil { // Check for errors
+		t.Error(err) // Log found error
+		t.FailNow()  // Panic
+	}
+
+	t.Logf("0x%s", decoded) // Log success
+}
+
+// TestBytes - test address to bytes conversion
+func TestBytes(t *testing.T) {
+	privateKey, err := ecdsa.GenerateKey(elliptic.P521(), rand.Reader)
+
+	if err != nil { // Check for errors
+		t.Error(err) // Log found error
+		t.FailNow()  // Panic
+	}
+
+	address, err := NewAddress(privateKey) // Generate address
+
+	if err != nil { // Check for errors
+		t.Error(err) // Log found error
+		t.FailNow()  // Panic
+	}
+
+	t.Log(address.Bytes()) // Log success
+}
+
 // TestString - test address to string conversion
 func TestString(t *testing.T) {
 	privateKey, err := ecdsa.GenerateKey(elliptic.P521(), rand.Reader)
@@ -25,3 +97,5 @@ func TestString(t *testing.T) {
 
 	t.Logf("0x%s", address.String()) // Log success
 }
+
+/* END EXPORTED METHODS */
