63fbea05f1a64d6463a5069d08aade5feeddd6df
Mitsuko Megumi <mitsukomegumii@gmail.com>
2018-12-02 00:56:33 -0500

    Init transaction signing.

--- types/transactionsigning.go
+++ types/transactionsigning.go
@@ -2,7 +2,10 @@ package types
 
 import (
 	"crypto/ecdsa"
+	"crypto/rand"
 	"math/big"
+
+	"github.com/space55/summertech-blockchain/crypto"
 )
 
 // signature - struct containing signature values
@@ -12,4 +15,28 @@ type signature struct {
 	V []byte   // Hash signature value
 	R *big.Int // Signature R
 	S *big.Int // Signature S
+}
+
+// SignTransaction - sign given transaction
+func SignTransaction(transaction *Transaction, privateKey *ecdsa.PrivateKey) error {
+	if transaction.Signature != nil { // Check not already signed
+		return ErrAlreadySigned // Return already signed error
+	}
+
+	r, s, err := ecdsa.Sign(rand.Reader, privateKey, crypto.Sha3(transaction.Bytes())) // Sign tx
+
+	if err != nil { // Check for errors
+		return err // Return found error
+	}
+
+	txSignature := signature{ // Initialize signature
+		PublicKey: &privateKey.PublicKey,            // Set public key
+		V:         crypto.Sha3(transaction.Bytes()), // Set val
+		R:         r,                                // Set R
+		S:         s,                                // Set S
+	}
+
+	*(*transaction).Signature = txSignature // Set signature
+
+	return nil // No error occurred, return nil
 }
