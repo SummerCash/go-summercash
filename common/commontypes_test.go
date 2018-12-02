b896e27bbc2444b0d558d1a6403d52bc226dbfc4
Mitsuko Megumi <mitsukomegumii@gmail.com>
2018-12-02 00:05:53 -0500

    Init hash primitive unit testing.

--- common/commontypes_test.go
+++ common/commontypes_test.go
@@ -5,9 +5,15 @@ import (
 	"crypto/elliptic"
 	"crypto/rand"
 	"testing"
+
+	"github.com/space55/summertech-blockchain/crypto"
 )
 
 /* BEGIN EXPORTED METHODS */
+
+/*
+	BEGIN ADDRESS METHODS
+*/
 
 // TestNewAddress - test init new address
 func TestNewAddress(t *testing.T) {
@@ -97,5 +103,79 @@ func TestString(t *testing.T) {
 
 	t.Log(address) // Log success
 }
+
+/*
+	END ADDRESS METHODS
+*/
+
+/*
+	BEGIN HASH METHODS
+*/
+
+// TestNewHash - test functionality of hash initializer
+func TestNewHash(t *testing.T) {
+	hash := NewHash(crypto.Sha3([]byte("test"))) // Init hash
+
+	if hash[:] == nil { // Check hash not nil
+		t.Errorf("invalid hash") // Log found error
+		t.FailNow()              // Panic
+	}
+
+	t.Log(hash) // Log success
+}
+
+// TestStringToHash - test functionality of StringToHash() method
+func TestStringToHash(t *testing.T) {
+	hash, err := StringToHash("0x307836f028580bb02cc8272a9a020f4200e346e276ae664e45ee80745574e2f5") // Decode to hash
+
+	if err != nil { // Check for errors
+		t.Error(err) // Log found error
+		t.FailNow()  // Panic
+	}
+
+	t.Log(hash) // Log decoded
+}
+
+// TestBytesHash - test functionality of hash bytes() extension method
+func TestBytesHash(t *testing.T) {
+	hash := NewHash(crypto.Sha3([]byte("test"))) // Init hash
+
+	if hash[:] == nil { // Check hash not nil
+		t.Errorf("invalid hash") // Log found error
+		t.FailNow()              // Panic
+	}
+
+	byteVal := hash.Bytes() // Get byte val
+
+	if byteVal == nil { // Check byte val not nil
+		t.Errorf("invalid hash") // Log found error
+		t.FailNow()              // Panic
+	}
+
+	t.Log(byteVal) // Log success
+}
+
+// TestStringHash - test functionality of hash string() extension method
+func TestStringHash(t *testing.T) {
+	hash := NewHash(crypto.Sha3([]byte("test"))) // Init hash
+
+	if hash[:] == nil { // Check hash not nil
+		t.Errorf("invalid hash") // Log found error
+		t.FailNow()              // Panic
+	}
+
+	stringVal := hash.String() // Get string val
+
+	if stringVal == "" { // Check byte val not nil
+		t.Errorf("invalid hash") // Log found error
+		t.FailNow()              // Panic
+	}
+
+	t.Log(stringVal) // Log success
+}
+
+/*
+	END HASH METHODS
+*/
 
 /* END EXPORTED METHODS */
