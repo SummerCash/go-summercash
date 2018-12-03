64a851ae6e6b747358641740350f4434b6f44f1f
Mitsuko Megumi <mitsukomegumii@gmail.com>
2018-12-03 12:31:09 -0500

    Moved crypto support files, updated proto compile script.

--- internal/rpc/compileProto.sh
+++ internal/rpc/compileProto.sh
@@ -3,6 +3,6 @@ read -p "proto file name (example: 'handler'): " name # Get file name
 
 mkdir $name # Init support folder
 
-cd $name # Cd into new folder
+cd proto && cd $name # Cd into new folder
 
-protoc --proto_path=$GOPATH/src:../$directory --twirp_out=. --go_out=. ../$directory/$name.proto # Compile proto file
+protoc --proto_path=$GOPATH/src:../../$directory --twirp_out=. --go_out=. ../../$directory/$name.proto # Compile proto file
