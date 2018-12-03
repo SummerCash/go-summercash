0f9f57fc9e086eae5508a75be3af273bbe9c22b7
Mitsuko Megumi <mitsukomegumii@gmail.com>
2018-12-03 13:00:40 -0500

    Sync...

--- cli/terminal.go
+++ cli/terminal.go
@@ -2,13 +2,17 @@ package cli
 
 import (
 	"bufio"
+	"context"
 	"crypto/tls"
 	"fmt"
 	"net/http"
 	"os"
+	"reflect"
+	"strconv"
 	"strings"
 
-	"github.com/mitsukomegumi/GoP2P/common"
+	"github.com/space55/summertech-blockchain/common"
+	cryptoProto "github.com/space55/summertech-blockchain/internal/rpc/proto/crypto"
 )
 
 // NewTerminal - attempts to start handler for term commands
@@ -38,4 +42,35 @@ func NewTerminal(rpcPort uint, rpcAddress string) {
 
 		handleCommand(receiver, methodname, params, rpcPort, rpcAddress, transport) // Handle command
 	}
+}
+
+// handleCommand - run handler for given receiver
+func handleCommand(receiver string, methodname string, params []string, rpcPort uint, rpcAddress string, transport *http.Transport) {
+	cryptoClient := cryptoProto.NewCryptoProtobufClient("https://"+rpcAddress+":"+strconv.Itoa(int(rpcPort)), &http.Client{Transport: transport}) // Init crypto client
+
+	switch receiver {
+	case "crypto":
+		err := handleCrypto(&cryptoClient, methodname, params) // Handle crypto
+
+		if err != nil { // Check for errors
+			fmt.Println("\n" + err.Error()) // Log found error
+		}
+	}
+}
+
+// handleCrypto - handle crypto receiver
+func handleCrypto(cryptoClient *cryptoProto.Crypto, methodname string, params []string) error {
+	reflectParams := []reflect.Value{} // Init buffer
+
+	reflectParams = append(reflectParams, reflect.ValueOf(context.Background())) // Append request context
+
+	// methodName - handle different methods
+	switch methodName {
+	case "Sha3", "Sha3String":
+		if len(params) != 1 { // Check for invalid params
+
+		}
+	}
+
+	return nil // No error occurred, return nil
 }
