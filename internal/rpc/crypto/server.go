adf337a19bd13e9a91cf04669959c2b3911cee8b
Mitsuko Megumi <mitsukomegumii@gmail.com>
2018-12-03 13:20:31 -0500

    Init crypto rpc server.

--- internal/rpc/crypto/server.go
+++ internal/rpc/crypto/server.go
@@ -1,1 +1,26 @@
 package crypto
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/space55/summertech-blockchain/crypto"
+	cryptoProto "github.com/space55/summertech-blockchain/internal/rpc/proto/crypto"
+)
+
+// Server - RPC server
+type Server struct{}
+
+// Sha3 - crypto.Sha3 RPC handler
+func (server *Server) Sha3(ctx context.Context, req *cryptoProto.GeneralRequest) (*cryptoProto.GeneralResponse, error) {
+	result := crypto.Sha3(req.Input) // Hash input
+
+	return &cryptoProto.GeneralResponse{Message: fmt.Sprintf("\n%s", result)}, nil // Return response
+}
+
+// Sha3String - crypto.Sha3String RPC handler
+func (server *Server) Sha3String(ctx context.Context, req *cryptoProto.GeneralRequest) (*cryptoProto.GeneralResponse, error) {
+	result := crypto.Sha3String(req.Input) // Hash input
+
+	return &cryptoProto.GeneralResponse{Message: fmt.Sprintf("\n%s", result)}, nil // Return response
+}
