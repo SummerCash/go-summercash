b92f520dd8b409b5d2db468fd4b641d562bc8238
Mitsuko Megumi <mitsukomegumii@gmail.com>
2018-12-07 11:08:52 -0500

    Init chainConfig unit testing.

--- config/chainconfig_test.go
+++ config/chainconfig_test.go
@@ -1,1 +1,15 @@
 package config
+
+import "testing"
+
+// TestNewChainConfig - test init method for chainConfigs
+func TestNewChainConfig(t *testing.T) {
+	chainConfig, err := NewChainConfig("genesis.json") // Initialize chain configuration
+
+	if err != nil { // Check for errors
+		t.Error(err) // Log found error
+		t.FailNow()  // Panic
+	}
+
+	t.Log(*chainConfig) // Log config
+}
