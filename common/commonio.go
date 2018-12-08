62c077b52b7aa46ed46b03e404d269115af62699
Mitsuko Megumi <mitsukomegumii@gmail.com>
2018-12-08 15:27:11 -0500

    Common: init gob i/o operations.

--- common/commonio.go
+++ common/commonio.go
@@ -1,6 +1,9 @@
 package common
 
-import "os"
+import (
+	"encoding/gob"
+	"os"
+)
 
 /* BEGIN EXPORTED METHODS */
 
@@ -15,6 +18,48 @@ func CreateDirIfDoesNotExit(dir string) error {
 	}
 
 	return nil // No error occurred
+}
+
+// WriteGob - create gob from specified object, at filePath
+func WriteGob(filePath string, object interface{}) error {
+	file, err := os.Create(filePath) // Attempt to create file at path
+
+	if err != nil { // Check for errors
+		return err // Return found error
+	}
+
+	encoder := gob.NewEncoder(file) // Write to file
+
+	err = encoder.Encode(object) // Encode object
+
+	if err != nil { // Check for errors
+		return err // Return found error
+	}
+
+	file.Close() // Close file operation
+
+	return err // Return error (might be nil)
+}
+
+// ReadGob - read gob specified at path
+func ReadGob(filePath string, object interface{}) error {
+	file, err := os.Open(filePath) // Attempt to open file at path
+
+	if err != nil { // Check for errors
+		return err // Return found error
+	}
+
+	decoder := gob.NewDecoder(file) // Attempt to decode gob
+
+	err = decoder.Decode(object) // Assign to error
+
+	if err != nil { // Check for errors
+		return err // Return found error
+	}
+
+	file.Close() // Close file
+
+	return err // Return error
 }
 
 /* END EXPORTED METHODS */
