e38927732a374ed49f8fa9619f62d5540ddd4f38
Mitsuko Megumi <mitsukomegumii@gmail.com>
2018-12-13 10:26:51 -0500

    Backup...

--- types/chain.go
+++ types/chain.go
@@ -16,6 +16,8 @@ type Chain struct {
 
 	Transactions []*Transaction `json:"transactions"` // Transactions in chain
 
+	Genesis common.Hash `json:"genesis"` // Genesis block hash
+
 	NetworkID uint        `json:"network"` // Network ID (mainnet: 0, testnet: 1, etc...)
 	ID        common.Hash `json:"ID"`      // Chain ID
 }
@@ -23,7 +25,12 @@ type Chain struct {
 var (
 	// ErrChainAlreadyExists - error definition describing a given chain that has already been registered in the coordinationChain
 	ErrChainAlreadyExists = errors.New("chain already exists for given account")
+
+	// ErrGenesisAlreadyExists - error definition describing a given chain with an existing genesis block
+	ErrGenesisAlreadyExists = errors.New("chain already has existing genesis")
 )
+
+/* BEGIN EXPORTED METHODS */
 
 // NewChain - initialize new chain
 func NewChain(account common.Address) (*Chain, error) {
@@ -77,3 +84,32 @@ func (chain *Chain) String() string {
 
 	return string(marshaled) // Return marshaled
 }
+
+/* END EXPORTED METHODS */
+
+/* BEGIN INTERNAL METHODS */
+
+// makeGenesis - generate genesis blocks from genesis file
+func (chain *Chain) makeGenesis(genesis *config.ChainConfig) (common.Hash, error) {
+	coordinationChain, err := ReadCoordinationChainFromMemory() // Read coordination chain from memory
+
+	if err != nil { // Check for errors
+		return common.Hash{}, err // Return error
+	}
+
+	genesisNode, _ := coordinationChain.GetGenesis() // Get genesis
+
+	if genesisNode != nil { // Check genesis already exists
+		return common.Hash{}, ErrGenesisAlreadyExists // Return error
+	}
+
+	genesisTx, err := NewTransaction(0, nil, nil, &genesis.AllocAddresses[0], genesis.Alloc[genesis.AllocAddresses[0].String()], []byte("genesis")) // Init transaction
+
+	if err != nil { // Check for errors
+		return common.Hash{}, err // Return error
+	}
+
+	return *genesisTx.Hash, nil // Return genesis
+}
+
+/* END INTERNAL METHODS */
