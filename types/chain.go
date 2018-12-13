66d4167ff8975e2690e329fb17f9becaf6153d11
Mitsuko Megumi <mitsukomegumii@gmail.com>
2018-12-13 18:00:52 -0500

    Init transaction appending.

--- types/chain.go
+++ types/chain.go
@@ -28,6 +28,9 @@ var (
 
 	// ErrGenesisAlreadyExists - error definition describing a given chain with an existing genesis block
 	ErrGenesisAlreadyExists = errors.New("chain already has existing genesis")
+
+	// ErrIrrelevantTransaction - error definition describing a transaction outside the scope of the given chain
+	ErrIrrelevantTransaction = errors.New("irrelevant transaction")
 )
 
 /* BEGIN EXPORTED METHODS */
@@ -54,19 +57,70 @@ func NewChain(account common.Address) (*Chain, error) {
 		return &Chain{}, ErrAlreadySigned // Return error
 	}
 
-	if coordinationChain.Nodes == nil || len(coordinationChain.Nodes) == 0 { // Check genesis
-
-	}
-
 	chain := &Chain{ // Init chain
 		Account:      account,
 		Transactions: []*Transaction{},
 		NetworkID:    config.NetworkID,
 	}
 
+	if coordinationChain.Nodes == nil || len(coordinationChain.Nodes) == 0 { // Check genesis
+		_, err := chain.makeGenesis(config) // Make genesis
+
+		if err != nil { // Check for errors
+			return &Chain{}, err // Return found error
+		}
+
+		// TODO: register on coordinationChain
+	}
+
 	(*chain).ID = common.NewHash(crypto.Sha3(chain.Bytes())) // Set ID
 
 	return chain, nil // Return initialized chain
+}
+
+// AddTransaction - append given transaction to chain
+func (chain *Chain) AddTransaction(transaction *Transaction) error {
+	if transaction.Signature == nil { // Check for nil signature
+		return ErrNilSignature // Return error
+	} else if *transaction.Recipient != chain.Account || *transaction.Sender != chain.Account { // Check irrelevant
+		return ErrIrrelevantTransaction // Return error
+	}
+
+	coordinationChain, err := ReadCoordinationChainFromMemory() // Read coordination chain from memory
+
+	if err != nil { // Check for errors
+		return err // Return error
+	}
+
+	signatureValid, err := VerifyTransactionSignature(transaction) // Verify signature
+
+	if err != nil { // Check for errors
+		return err // Return found error
+	} else if signatureValid != true { // Check bad signature
+		return ErrInvalidSignature // Return error
+	}
+
+	if transaction.Recipient == nil || transaction.Recipient.String() == "" { // Check nil recipient
+		return ErrNilAddress // Return error
+	} else if transaction.Sender == nil && chain.Genesis != (common.Hash{}) { // Check genesis already exists
+		return ErrGenesisAlreadyExists // Return error
+	}
+
+	_, err = coordinationChain.GetGenesis() // Get genesis
+
+	if err == nil && transaction.Sender == nil { // Check genesis already exists
+		return ErrGenesisAlreadyExists // Return error
+	}
+
+	if len(chain.Transactions) == 0 && transaction.Sender == nil { // Check is genesis
+		chain.Genesis = *transaction.Hash // Set genesis
+	} else if len(chain.Transactions) == 0 { // Check first index
+		chain.Transactions = []*Transaction{transaction} // Set transaction
+	} else {
+		chain.Transactions = append(chain.Transactions, transaction) // Append transaction
+	}
+
+	return nil // No error occurred, return nil
 }
 
 // Bytes - convert given chain to byte array
@@ -107,6 +161,16 @@ func (chain *Chain) makeGenesis(genesis *config.ChainConfig) (common.Hash, error
 
 	if err != nil { // Check for errors
 		return common.Hash{}, err // Return error
+	}
+
+	lastTx := genesisTx // Set initial
+
+	for x := 1; x != len(genesis.AllocAddresses); x++ { // Iterate through allocations
+		lastTx, err = NewTransaction(uint64(x+1), lastTx, nil, &genesis.AllocAddresses[x], genesis.Alloc[genesis.AllocAddresses[x].String()], []byte("genesisChild")) // Init transaction
+
+		if err != nil { // Check for errors
+			return common.Hash{}, err // Return error
+		}
 	}
 
 	return *genesisTx.Hash, nil // Return genesis
