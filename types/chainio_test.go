80dffcfc1932d971585b2c6caf6cfdddd6936c1d
Mitsuko Megumi <mitsukomegumii@gmail.com>
2018-12-13 20:20:20 -0500

    Init chain io.

---
+++ types/chainio_test.go
@@ -0,0 +1,45 @@
+package types
+
+import (
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"testing"
+
+	"github.com/space55/summertech-blockchain/common"
+)
+
+/* BEGIN EXPORTED METHODS */
+
+// TestWriteChainToMemory - test chain writeToMemory() method
+func TestWriteChainToMemory(t *testing.T) {
+	privateKey, err := ecdsa.GenerateKey(elliptic.P521(), rand.Reader) // Generate private key
+
+	if err != nil { // Check for errors
+		t.Error(err) // Log found error
+		t.FailNow()  // Panic
+	}
+
+	address, err := common.NewAddress(privateKey) // Generate address
+
+	if err != nil { // Check for errors
+		t.Error(err) // Log found error
+		t.FailNow()  // Panic
+	}
+
+	chain, err := NewChain(address) // Initialize chain
+
+	if err != nil { // Check for errors
+		t.Error(err) // Log found error
+		t.FailNow()  // Panic
+	}
+
+	err = chain.WriteToMemory() // Write chain to memory
+
+	if err != nil { // Check for errors
+		t.Error(err) // Log found error
+		t.FailNow()  // Panic
+	}
+
+	t.Log("success") // Log success
+}
