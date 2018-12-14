cd669878c5299c98d26bc24eff4c54bf50c0ef9a
Mitsuko Megumi <mitsukomegumii@gmail.com>
2018-12-14 08:05:02 -0500

    Gitignored keystore.

--- common/commontypesio.go
+++ common/commontypesio.go
@@ -32,7 +32,7 @@ func (address *Address) WriteToMemory(privateKey *ecdsa.PrivateKey) error {
 
 	pemEncoded := pem.EncodeToMemory(&pem.Block{Type: "PRIVATE KEY", Bytes: marshaledPrivateKey}) // Encode to memory
 
-	err = ioutil.WriteFile(fmt.Sprintf("%s/keystore/key_%s.gob", DataDir, address.String()), pemEncoded, 0644) // Write encoded private key
+	err = ioutil.WriteFile(fmt.Sprintf("%s/keystore/key_%s.pem", DataDir, address.String()), pemEncoded, 0644) // Write encoded private key
 
 	if err != nil { // Check for errors
 		return err // Return found error
@@ -43,12 +43,18 @@ func (address *Address) WriteToMemory(privateKey *ecdsa.PrivateKey) error {
 
 // ReadAccountFromMemory - attempt to retrieve given address, keystore from persistent storage
 func ReadAccountFromMemory(address *Address) (*Address, *ecdsa.PrivateKey, error) {
-	privateKey := &ecdsa.PrivateKey{} // Init buffer
-
-	err := ReadGob(fmt.Sprintf("%s/keystore/key_%s.gob", DataDir, address.String()), privateKey) // Read private key
+	data, err := ioutil.ReadFile(fmt.Sprintf("%s/keystore/key_%s.pem", DataDir, address.String())) // Read encoded PEM
 
 	if err != nil { // Check for errors
-		return &Address{}, &ecdsa.PrivateKey{}, err // Return error
+		return &Address{}, &ecdsa.PrivateKey{}, err // Return found error
+	}
+
+	pemEncoded, _ := pem.Decode(data) // Decode data
+
+	privateKey, err := x509.ParseECPrivateKey(pemEncoded.Bytes) // Decrypt pem
+
+	if err != nil { // Check for errors
+		return &Address{}, &ecdsa.PrivateKey{}, err // Return found error
 	}
 
 	return address, privateKey, nil // No error occurred, return read addr, private key
--- common/commontypesio_test.go
+++ common/commontypesio_test.go
@@ -35,6 +35,40 @@ func TestWriteToMemory(t *testing.T) {
 	t.Log("success") // Log success
 }
 
+// TestReadFromMemory - test reading of account from persistent storage
+func TestReadFromMemory(t *testing.T) {
+	privateKey, err := ecdsa.GenerateKey(elliptic.P521(), rand.Reader) // Generate private key
+
+	if err != nil { // Check for errors
+		t.Error(err) // Log found error
+		t.FailNow()  // Panic
+	}
+
+	address, err := NewAddress(privateKey) // Generate address
+
+	if err != nil { // Check for errors
+		t.Error(err) // Log found error
+		t.FailNow()  // Panic
+	}
+
+	err = address.WriteToMemory(privateKey) // Write to memory
+
+	if err != nil { // Check for errors
+		t.Error(err) // Log found error
+		t.FailNow()  // Panic
+	}
+
+	readAddress, privateKey, err := ReadAccountFromMemory(&address) // Read account from memory
+
+	if err != nil { // Check for errors
+		t.Error(err) // Log found error
+		t.FailNow()  // Panic
+	}
+
+	t.Log(privateKey)           // Log success
+	t.Log(readAddress.String()) // Log success
+}
+
 /* END EXPORTED METHODS */
 
 /* BEGIN INTERNAL METHODS */
