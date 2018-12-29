1c8461faf3ab4fe28e36b6d113df3d4669fab1d8
Dowland Aiello <mitsukomegumii@gmail.com>
2018-12-29 09:58:35 -0500

    Finished transactions RPC server!

--- internal/rpc/transaction/server.go
+++ internal/rpc/transaction/server.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/space55/summertech-blockchain/accounts"
 	"github.com/space55/summertech-blockchain/common"
 	transactionProto "github.com/space55/summertech-blockchain/internal/rpc/proto/transaction"
 	"github.com/space55/summertech-blockchain/types"
@@ -51,6 +52,12 @@ func (server *Server) NewTransaction(ctx context.Context, req *transactionProto.
 		return &transactionProto.GeneralResponse{}, err // Return found error
 	}
 
+	err = transaction.WriteToMemory() // Write transaction to persistent memory
+
+	if err != nil { // Check for errors
+		return &transactionProto.GeneralResponse{}, err // Return found error
+	}
+
 	return &transactionProto.GeneralResponse{Message: fmt.Sprintf("\n%s", transaction.String())}, nil // Return response
 }
 
@@ -63,4 +70,125 @@ func (server *Server) TransactionFromBytes(ctx context.Context, req *transaction
 	}
 
 	return &transactionProto.GeneralResponse{Message: fmt.Sprintf("\n%s", transaction.String())}, nil // Return response
+}
+
+// Publish - transaction.Publish RPC handler
+func (server *Server) Publish(ctx context.Context, req *transactionProto.GeneralRequest) (*transactionProto.GeneralResponse, error) {
+	hash, err := common.StringToHash(req.Address) // String to hash
+
+	if err != nil { // Check for errors
+		return &transactionProto.GeneralResponse{}, err // Return found error
+	}
+
+	transaction, err := types.ReadTransactionFromMemory(hash) // Read transaction from hash
+
+	if err != nil { // Check for errors
+		return &transactionProto.GeneralResponse{}, err // Return found error
+	}
+
+	err = transaction.Publish() // Publish transaction
+
+	if err != nil { // Check for errors
+		return &transactionProto.GeneralResponse{}, err // Return found error
+	}
+
+	return &transactionProto.GeneralResponse{Message: fmt.Sprintf("\npublished transaction %s", transaction.Hash)}, nil // Return response
+}
+
+// Bytes - transaction.Bytes RPC handler
+func (server *Server) Bytes(ctx context.Context, req *transactionProto.GeneralRequest) (*transactionProto.GeneralResponse, error) {
+	hash, err := common.StringToHash(req.Address) // String to hash
+
+	if err != nil { // Check for errors
+		return &transactionProto.GeneralResponse{}, err // Return found error
+	}
+
+	transaction, err := types.ReadTransactionFromMemory(hash) // Read transaction from hash
+
+	if err != nil { // Check for errors
+		return &transactionProto.GeneralResponse{}, err // Return found error
+	}
+
+	hex, err := common.EncodeString(transaction.Bytes()) // Encode byte value
+
+	if err != nil { // Check for errors
+		return &transactionProto.GeneralResponse{}, err // Return found error
+	}
+
+	return &transactionProto.GeneralResponse{Message: fmt.Sprintf("\n%s", hex)}, nil // Return response
+}
+
+// String - transaction.String RPC handler
+func (server *Server) String(ctx context.Context, req *transactionProto.GeneralRequest) (*transactionProto.GeneralResponse, error) {
+	hash, err := common.StringToHash(req.Address) // String to hash
+
+	if err != nil { // Check for errors
+		return &transactionProto.GeneralResponse{}, err // Return found error
+	}
+
+	transaction, err := types.ReadTransactionFromMemory(hash) // Read transaction from hash
+
+	if err != nil { // Check for errors
+		return &transactionProto.GeneralResponse{}, err // Return found error
+	}
+
+	return &transactionProto.GeneralResponse{Message: fmt.Sprintf("\n%s", transaction.String())}, nil // Return response
+}
+
+// SignTransaction - transaction.SignTransaction RPC handler
+func (server *Server) SignTransaction(ctx context.Context, req *transactionProto.GeneralRequest) (*transactionProto.GeneralResponse, error) {
+	hash, err := common.StringToHash(req.Address) // String to hash
+
+	if err != nil { // Check for errors
+		return &transactionProto.GeneralResponse{}, err // Return found error
+	}
+
+	transaction, err := types.ReadTransactionFromMemory(hash) // Read transaction from hash
+
+	if err != nil { // Check for errors
+		return &transactionProto.GeneralResponse{}, err // Return found error
+	}
+
+	account, err := accounts.ReadAccountFromMemory(*transaction.Sender) // Read account
+
+	if err != nil { // Check for errors
+		return &transactionProto.GeneralResponse{}, err // Return found error
+	}
+
+	err = types.SignTransaction(transaction, account.PrivateKey) // Sign transaction
+
+	if err != nil { // Check for errors
+		return &transactionProto.GeneralResponse{}, err // Return found error
+	}
+
+	err = transaction.WriteToMemory() // Write to memory
+
+	if err != nil { // Check for errors
+		return &transactionProto.GeneralResponse{}, err // Return found error
+	}
+
+	return &transactionProto.GeneralResponse{Message: fmt.Sprintf("\n%s", transaction.Signature.String())}, nil // Return response
+}
+
+// VerifyTransactionSignature - transaction.VerifyTransactionSignature RPC handler
+func (server *Server) VerifyTransactionSignature(ctx context.Context, req *transactionProto.GeneralRequest) (*transactionProto.GeneralResponse, error) {
+	hash, err := common.StringToHash(req.Address) // Get hash value
+
+	if err != nil { // Check for errors
+		return &transactionProto.GeneralResponse{}, err // Return found error
+	}
+
+	transaction, err := types.ReadTransactionFromMemory(hash) // Read transaction from mempool
+
+	if err != nil { // Check for errors
+		return &transactionProto.GeneralResponse{}, err // Return found error
+	}
+
+	verified, err := types.VerifyTransactionSignature(transaction) // Verify signature
+
+	if err != nil { // Check for errors
+		return &transactionProto.GeneralResponse{}, err // Return found error
+	}
+
+	return &transactionProto.GeneralResponse{Message: fmt.Sprintf("\n%t", verified)}, nil // Return response
 }
