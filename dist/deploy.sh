2b190e5861aa009fc943167453604567fa3d7b79
Dowland Aiello <mitsukomegumii@gmail.com>
2019-01-01 15:11:37 -0500

    Deployed

--- dist/deploy.sh
+++ dist/deploy.sh
@@ -1,8 +1,22 @@
 # USAGE: ./deploy.sh PLATFORM-NAME (e.g. ./deploy.sh macOS)
 
 cd ..
+
 go build
+
 mv go-summercash dist/go-summercash-$1
+
 git add .
 git commit -m "Deployed"
 git push
+
+cd dist
+
+if [ "$1" = "macOS" ]
+then
+    zip go-summercash-$1.zip go-summercash-$1
+elif [ "$1" = "win64" ]
+then
+    zip go-summercash-$1.zip go-summercash-$1.exe
+else
+    tar -cvzf go-summercash-$1.tar.gz go-summercash-$1
