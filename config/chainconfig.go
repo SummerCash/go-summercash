86638d1983d16bef851e8efa61be77e6e76a8f55
Dowland Aiello <mitsukomegumii@gmail.com>
2019-01-02 13:45:44 -0500

    Updated version def.

--- config/chainconfig.go
+++ config/chainconfig.go
@@ -25,7 +25,7 @@ type ChainConfig struct {
 
 const (
 	// Version - dist version def
-	Version = "0.2.0"
+	Version = "0.3.0"
 )
 
 // NewChainConfig - generate new ChainConfig from genesis.json file
