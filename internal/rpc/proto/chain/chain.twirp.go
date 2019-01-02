61fcfe269943750510c775a16a251911ba4fbd84
Dowland Aiello <mitsukomegumii@gmail.com>
2019-01-02 11:11:07 -0500

    Recompiled chain proto.

--- internal/rpc/proto/chain/chain.twirp.go
+++ internal/rpc/proto/chain/chain.twirp.go
@@ -42,6 +42,8 @@ type Chain interface {
 	ReadChainFromMemory(context.Context, *GeneralRequest) (*GeneralResponse, error)
 
 	QueryTransaction(context.Context, *GeneralRequest) (*GeneralResponse, error)
+
+	GetNumTransactions(context.Context, *GeneralRequest) (*GeneralResponse, error)
 }
 
 // =====================
@@ -50,19 +52,20 @@ type Chain interface {
 
 type chainProtobufClient struct {
 	client HTTPClient
-	urls   [5]string
+	urls   [6]string
 }
 
 // NewChainProtobufClient creates a Protobuf client that implements the Chain interface.
 // It communicates using Protobuf and can be configured with a custom HTTPClient.
 func NewChainProtobufClient(addr string, client HTTPClient) Chain {
 	prefix := urlBase(addr) + ChainPathPrefix
-	urls := [5]string{
+	urls := [6]string{
 		prefix + "GetBalance",
 		prefix + "Bytes",
 		prefix + "String",
 		prefix + "ReadChainFromMemory",
 		prefix + "QueryTransaction",
+		prefix + "GetNumTransactions",
 	}
 	if httpClient, ok := client.(*http.Client); ok {
 		return &chainProtobufClient{
@@ -136,25 +139,38 @@ func (c *chainProtobufClient) QueryTransaction(ctx context.Context, in *GeneralR
 	return out, nil
 }
 
+func (c *chainProtobufClient) GetNumTransactions(ctx context.Context, in *GeneralRequest) (*GeneralResponse, error) {
+	ctx = ctxsetters.WithPackageName(ctx, "chain")
+	ctx = ctxsetters.WithServiceName(ctx, "Chain")
+	ctx = ctxsetters.WithMethodName(ctx, "GetNumTransactions")
+	out := new(GeneralResponse)
+	err := doProtobufRequest(ctx, c.client, c.urls[5], in, out)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // =================
 // Chain JSON Client
 // =================
 
 type chainJSONClient struct {
 	client HTTPClient
-	urls   [5]string
+	urls   [6]string
 }
 
 // NewChainJSONClient creates a JSON client that implements the Chain interface.
 // It communicates using JSON and can be configured with a custom HTTPClient.
 func NewChainJSONClient(addr string, client HTTPClient) Chain {
 	prefix := urlBase(addr) + ChainPathPrefix
-	urls := [5]string{
+	urls := [6]string{
 		prefix + "GetBalance",
 		prefix + "Bytes",
 		prefix + "String",
 		prefix + "ReadChainFromMemory",
 		prefix + "QueryTransaction",
+		prefix + "GetNumTransactions",
 	}
 	if httpClient, ok := client.(*http.Client); ok {
 		return &chainJSONClient{
@@ -228,6 +244,18 @@ func (c *chainJSONClient) QueryTransaction(ctx context.Context, in *GeneralReque
 	return out, nil
 }
 
+func (c *chainJSONClient) GetNumTransactions(ctx context.Context, in *GeneralRequest) (*GeneralResponse, error) {
+	ctx = ctxsetters.WithPackageName(ctx, "chain")
+	ctx = ctxsetters.WithServiceName(ctx, "Chain")
+	ctx = ctxsetters.WithMethodName(ctx, "GetNumTransactions")
+	out := new(GeneralResponse)
+	err := doJSONRequest(ctx, c.client, c.urls[5], in, out)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // ====================
 // Chain Server Handler
 // ====================
@@ -290,6 +318,9 @@ func (s *chainServer) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
 		return
 	case "/twirp/chain.Chain/QueryTransaction":
 		s.serveQueryTransaction(ctx, resp, req)
+		return
+	case "/twirp/chain.Chain/GetNumTransactions":
+		s.serveGetNumTransactions(ctx, resp, req)
 		return
 	default:
 		msg := fmt.Sprintf("no handler for path %q", req.URL.Path)
@@ -1019,6 +1050,150 @@ func (s *chainServer) serveQueryTransactionProtobuf(ctx context.Context, resp ht
 	callResponseSent(ctx, s.hooks)
 }
 
+func (s *chainServer) serveGetNumTransactions(ctx context.Context, resp http.ResponseWriter, req *http.Request) {
+	header := req.Header.Get("Content-Type")
+	i := strings.Index(header, ";")
+	if i == -1 {
+		i = len(header)
+	}
+	switch strings.TrimSpace(strings.ToLower(header[:i])) {
+	case "application/json":
+		s.serveGetNumTransactionsJSON(ctx, resp, req)
+	case "application/protobuf":
+		s.serveGetNumTransactionsProtobuf(ctx, resp, req)
+	default:
+		msg := fmt.Sprintf("unexpected Content-Type: %q", req.Header.Get("Content-Type"))
+		twerr := badRouteError(msg, req.Method, req.URL.Path)
+		s.writeError(ctx, resp, twerr)
+	}
+}
+
+func (s *chainServer) serveGetNumTransactionsJSON(ctx context.Context, resp http.ResponseWriter, req *http.Request) {
+	var err error
+	ctx = ctxsetters.WithMethodName(ctx, "GetNumTransactions")
+	ctx, err = callRequestRouted(ctx, s.hooks)
+	if err != nil {
+		s.writeError(ctx, resp, err)
+		return
+	}
+
+	reqContent := new(GeneralRequest)
+	unmarshaler := jsonpb.Unmarshaler{AllowUnknownFields: true}
+	if err = unmarshaler.Unmarshal(req.Body, reqContent); err != nil {
+		err = wrapErr(err, "failed to parse request json")
+		s.writeError(ctx, resp, twirp.InternalErrorWith(err))
+		return
+	}
+
+	// Call service method
+	var respContent *GeneralResponse
+	func() {
+		defer func() {
+			// In case of a panic, serve a 500 error and then panic.
+			if r := recover(); r != nil {
+				s.writeError(ctx, resp, twirp.InternalError("Internal service panic"))
+				panic(r)
+			}
+		}()
+		respContent, err = s.Chain.GetNumTransactions(ctx, reqContent)
+	}()
+
+	if err != nil {
+		s.writeError(ctx, resp, err)
+		return
+	}
+	if respContent == nil {
+		s.writeError(ctx, resp, twirp.InternalError("received a nil *GeneralResponse and nil error while calling GetNumTransactions. nil responses are not supported"))
+		return
+	}
+
+	ctx = callResponsePrepared(ctx, s.hooks)
+
+	var buf bytes.Buffer
+	marshaler := &jsonpb.Marshaler{OrigName: true}
+	if err = marshaler.Marshal(&buf, respContent); err != nil {
+		err = wrapErr(err, "failed to marshal json response")
+		s.writeError(ctx, resp, twirp.InternalErrorWith(err))
+		return
+	}
+
+	ctx = ctxsetters.WithStatusCode(ctx, http.StatusOK)
+	resp.Header().Set("Content-Type", "application/json")
+	resp.WriteHeader(http.StatusOK)
+
+	respBytes := buf.Bytes()
+	if n, err := resp.Write(respBytes); err != nil {
+		msg := fmt.Sprintf("failed to write response, %d of %d bytes written: %s", n, len(respBytes), err.Error())
+		twerr := twirp.NewError(twirp.Unknown, msg)
+		callError(ctx, s.hooks, twerr)
+	}
+	callResponseSent(ctx, s.hooks)
+}
+
+func (s *chainServer) serveGetNumTransactionsProtobuf(ctx context.Context, resp http.ResponseWriter, req *http.Request) {
+	var err error
+	ctx = ctxsetters.WithMethodName(ctx, "GetNumTransactions")
+	ctx, err = callRequestRouted(ctx, s.hooks)
+	if err != nil {
+		s.writeError(ctx, resp, err)
+		return
+	}
+
+	buf, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		err = wrapErr(err, "failed to read request body")
+		s.writeError(ctx, resp, twirp.InternalErrorWith(err))
+		return
+	}
+	reqContent := new(GeneralRequest)
+	if err = proto.Unmarshal(buf, reqContent); err != nil {
+		err = wrapErr(err, "failed to parse request proto")
+		s.writeError(ctx, resp, twirp.InternalErrorWith(err))
+		return
+	}
+
+	// Call service method
+	var respContent *GeneralResponse
+	func() {
+		defer func() {
+			// In case of a panic, serve a 500 error and then panic.
+			if r := recover(); r != nil {
+				s.writeError(ctx, resp, twirp.InternalError("Internal service panic"))
+				panic(r)
+			}
+		}()
+		respContent, err = s.Chain.GetNumTransactions(ctx, reqContent)
+	}()
+
+	if err != nil {
+		s.writeError(ctx, resp, err)
+		return
+	}
+	if respContent == nil {
+		s.writeError(ctx, resp, twirp.InternalError("received a nil *GeneralResponse and nil error while calling GetNumTransactions. nil responses are not supported"))
+		return
+	}
+
+	ctx = callResponsePrepared(ctx, s.hooks)
+
+	respBytes, err := proto.Marshal(respContent)
+	if err != nil {
+		err = wrapErr(err, "failed to marshal proto response")
+		s.writeError(ctx, resp, twirp.InternalErrorWith(err))
+		return
+	}
+
+	ctx = ctxsetters.WithStatusCode(ctx, http.StatusOK)
+	resp.Header().Set("Content-Type", "application/protobuf")
+	resp.WriteHeader(http.StatusOK)
+	if n, err := resp.Write(respBytes); err != nil {
+		msg := fmt.Sprintf("failed to write response, %d of %d bytes written: %s", n, len(respBytes), err.Error())
+		twerr := twirp.NewError(twirp.Unknown, msg)
+		callError(ctx, s.hooks, twerr)
+	}
+	callResponseSent(ctx, s.hooks)
+}
+
 func (s *chainServer) ServiceDescriptor() ([]byte, int) {
 	return twirpFileDescriptor0, 0
 }
@@ -1448,19 +1623,20 @@ func callError(ctx context.Context, h *twirp.ServerHooks, err twirp.Error) conte
 }
 
 var twirpFileDescriptor0 = []byte{
-	// 213 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0xe2, 0x4e, 0xce, 0x48, 0xcc,
-	0xcc, 0xd3, 0x2b, 0x28, 0xca, 0x2f, 0xc9, 0x17, 0x62, 0x05, 0x73, 0x94, 0xb4, 0xb8, 0xf8, 0xdc,
-	0x53, 0xf3, 0x52, 0x8b, 0x12, 0x73, 0x82, 0x52, 0x0b, 0x4b, 0x53, 0x8b, 0x4b, 0x84, 0x24, 0xb8,
-	0xd8, 0x13, 0x53, 0x52, 0x8a, 0x52, 0x8b, 0x8b, 0x25, 0x18, 0x15, 0x18, 0x35, 0x38, 0x83, 0x60,
-	0x5c, 0x25, 0x6d, 0x2e, 0x7e, 0xb8, 0xda, 0xe2, 0x82, 0xfc, 0xbc, 0xe2, 0x54, 0x90, 0xe2, 0xdc,
-	0xd4, 0xe2, 0xe2, 0xc4, 0xf4, 0x54, 0x98, 0x62, 0x28, 0xd7, 0xe8, 0x04, 0x13, 0x17, 0xab, 0x33,
-	0xc8, 0x0a, 0x21, 0x5b, 0x2e, 0x2e, 0xf7, 0xd4, 0x12, 0xa7, 0xc4, 0x9c, 0xc4, 0xbc, 0xe4, 0x54,
-	0x21, 0x51, 0x3d, 0x88, 0x2b, 0x50, 0x6d, 0x95, 0x12, 0x43, 0x17, 0x86, 0x58, 0xa0, 0xc4, 0x20,
-	0x64, 0xc1, 0xc5, 0xea, 0x54, 0x59, 0x92, 0x5a, 0x4c, 0xba, 0x4e, 0x4b, 0x2e, 0xb6, 0xe0, 0x92,
-	0xa2, 0xcc, 0xbc, 0x74, 0xd2, 0xb5, 0xba, 0x71, 0x09, 0x07, 0xa5, 0x26, 0xa6, 0x80, 0x3d, 0xe0,
-	0x56, 0x94, 0x9f, 0xeb, 0x9b, 0x9a, 0x9b, 0x5f, 0x54, 0x49, 0xba, 0x39, 0xce, 0x5c, 0x02, 0x81,
-	0xa5, 0xa9, 0x45, 0x95, 0x21, 0x45, 0x89, 0x79, 0xc5, 0x89, 0xc9, 0x25, 0x99, 0xf9, 0x79, 0x24,
-	0x1b, 0x92, 0xc4, 0x06, 0x8e, 0x31, 0x63, 0x40, 0x00, 0x00, 0x00, 0xff, 0xff, 0x72, 0x2b, 0x57,
-	0x48, 0xc0, 0x01, 0x00, 0x00,
+	// 226 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x94, 0x91, 0x41, 0x4b, 0x87, 0x40,
+	0x10, 0xc5, 0xfb, 0x13, 0x1a, 0x4d, 0x50, 0xb1, 0x51, 0x48, 0xa7, 0xf0, 0x14, 0x05, 0x1e, 0xea,
+	0x52, 0x87, 0x2e, 0x4a, 0x7a, 0x2a, 0xc8, 0xfa, 0x02, 0x93, 0x0e, 0x26, 0xb8, 0xbb, 0x36, 0xb3,
+	0x1e, 0xfc, 0x14, 0x7d, 0xe5, 0x50, 0x33, 0xea, 0x7f, 0xdb, 0xe3, 0x5b, 0xde, 0x7b, 0xbf, 0x9d,
+	0x19, 0x38, 0xa8, 0x3e, 0xb0, 0x35, 0x49, 0xcf, 0xd6, 0x59, 0x15, 0xcc, 0x22, 0xbe, 0x82, 0xc3,
+	0x82, 0x0c, 0x31, 0x76, 0x25, 0x7d, 0x0e, 0x24, 0x4e, 0x45, 0xb0, 0x87, 0x75, 0xcd, 0x24, 0x12,
+	0x6d, 0x2e, 0x36, 0x97, 0xfb, 0xe5, 0x2a, 0xe3, 0x6b, 0x38, 0xfa, 0xf5, 0x4a, 0x6f, 0x8d, 0xd0,
+	0x64, 0xd6, 0x24, 0x82, 0x0d, 0xad, 0xe6, 0x1f, 0x79, 0xf3, 0xb5, 0x0b, 0x41, 0x36, 0x21, 0xd4,
+	0x03, 0x40, 0x41, 0x2e, 0xc5, 0x0e, 0x4d, 0x45, 0xea, 0x34, 0x59, 0x7e, 0xf1, 0x9f, 0x7a, 0x7e,
+	0xb6, 0xfd, 0xbc, 0x00, 0xe2, 0x1d, 0x75, 0x07, 0x41, 0x3a, 0x3a, 0x12, 0xff, 0xe4, 0x3d, 0x84,
+	0xaf, 0x8e, 0x5b, 0xd3, 0xf8, 0x47, 0x73, 0x38, 0x29, 0x09, 0xeb, 0x79, 0x80, 0x9c, 0xad, 0x7e,
+	0x22, 0x6d, 0x79, 0xf4, 0xef, 0xc9, 0xe0, 0xf8, 0x65, 0x20, 0x1e, 0xdf, 0x18, 0x8d, 0x60, 0xe5,
+	0x5a, 0x6b, 0xfc, 0x4b, 0x1e, 0x41, 0x15, 0xe4, 0x9e, 0x07, 0xfd, 0xa7, 0xc5, 0x7f, 0x1d, 0xef,
+	0xe1, 0x7c, 0xf8, 0xdb, 0xef, 0x00, 0x00, 0x00, 0xff, 0xff, 0x6c, 0xe7, 0x3f, 0x86, 0x07, 0x02,
+	0x00, 0x00,
 }
