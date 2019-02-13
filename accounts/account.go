5bac5cb0aea51b3412caf9016ecc28d618127e50
Dowland Aiello <mitsukomegumii@gmail.com>
2019-02-13 16:14:57 -0500

    Nvmd.

--- accounts/account.go
+++ accounts/account.go
@@ -162,8 +162,6 @@ func GetAllContracts(deployingAccount common.Address) ([]string, error) {
 			return []string{}, err // Return found error
 		}
 
-		panic(len(chain.Transactions))
-
 		if chain.ContractSource != nil && *chain.Transactions[0].Sender == deployingAccount { // Check is contract from account
 			buffer = append(buffer, strings.Split(strings.Split(file.Name(), "chain_")[1], ".json")[0]) // Append to buffer
 		}
