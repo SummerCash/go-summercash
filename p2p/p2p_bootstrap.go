f25755722659a5b6bd1a45a11fe980056f2bc58c
Dowland Aiello <mitsukomegumii@gmail.com>
2019-03-26 13:11:28 -0400

    p2p: Added bootstrap addr.

--- p2p/p2p_bootstrap.go
+++ p2p/p2p_bootstrap.go
@@ -1,2 +1,9 @@
 // Package p2p outlines helper methods and types for p2p communications.
 package p2p
+
+var (
+	// BootstrapNodes represents all default bootstrap nodes on the given network.
+	BootstrapNodes = []string{
+		"/ip4/108.41.124.60/udp/3000/ipfs/QmeUXECRX2awd3WeJ7xVvaYJ8u1UGKDoiPSS4XTcTBZ5YF",
+	}
+)
