f94d40517a3637e641244862eac8f2d450c92bda
Dowland Aiello <mitsukomegumii@gmail.com>
2019-03-26 13:11:47 -0400

    p2p: Added NewHost() functionality.

--- p2p/p2p_host.go
+++ p2p/p2p_host.go
@@ -5,11 +5,20 @@ import (
 	"context"
 	"strconv"
 
+	"github.com/SummerCash/go-summercash/common"
+
 	"github.com/libp2p/go-libp2p"
 	host "github.com/libp2p/go-libp2p-host"
 	dht "github.com/libp2p/go-libp2p-kad-dht"
+	peerstore "github.com/libp2p/go-libp2p-peerstore"
 	quic "github.com/libp2p/go-libp2p-quic-transport"
 	routed "github.com/libp2p/go-libp2p/p2p/host/routed"
+	multiaddr "github.com/multiformats/go-multiaddr"
+)
+
+var (
+	// WorkingHost represents the global routed host.
+	WorkingHost *routed.RoutedHost
 )
 
 /* BEGIN EXPORTED METHODS */
@@ -44,6 +53,14 @@ func NewHost(ctx context.Context, port int) (*routed.RoutedHost, error) {
 	if err != nil { // Check for errors
 		return &routed.RoutedHost{}, err // Return found error
 	}
+
+	routedHost := routed.Wrap(host, dht) // Wrap host with DHT
+
+	WorkingHost = routedHost // Set working host
+
+	common.Logf("== P2P == initialized host with ID: %s on listening port: %d with multiaddr: %s", host.ID().Pretty(), strconv.Itoa(port), host.Addrs()[0].String()) // Log host
+
+	return WorkingHost, nil // Return working routed host
 }
 
 // BootstrapDht bootstraps a KadDht to the list of bootstrap nodes.
@@ -60,9 +77,27 @@ func BootstrapDht(ctx context.Context, host host.Host) (*dht.IpfsDHT, error) {
 		return nil, err // Return found error
 	}
 
-	for _, addr := range BootstrapNodes {
+	for _, addr := range BootstrapNodes { // Iterate through bootstrap nodes
+		address, err := multiaddr.NewMultiaddr(addr) // Parse multi address
 
+		if err != nil { // Check for errors
+			continue // Continue to next peer
+		}
+
+		peerInfo, err := peerstore.InfoFromP2pAddr(address) // Get peer info
+
+		if err != nil { // Check for errors
+			continue // Continue to next peer
+		}
+
+		err = host.Connect(ctx, *peerInfo) // Connect to discovered peer
+
+		if err != nil { // Check for errors
+			continue // Continue to next peer
+		}
 	}
+
+	return dht, nil // Return DHT
 }
 
 /* END EXPORTED METHODS */
