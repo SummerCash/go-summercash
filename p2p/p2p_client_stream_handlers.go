f77870019857df265912bbfc99b0d9cd8f51aaf9
Dowland Aiello <mitsukomegumii@gmail.com>
2019-03-31 01:50:29 -0400

    p2p: Another test...

--- p2p/p2p_client_stream_handlers.go
+++ p2p/p2p_client_stream_handlers.go
@@ -127,9 +127,11 @@ func (client *Client) HandleReceiveBestTransaction(stream inet.Stream) {
 
 	if len(chain.Transactions) > 0 { // Check has txs
 		fmt.Println(hex.EncodeToString(chain.Transactions[len(chain.Transactions)-1].Hash.Bytes()))
+		fmt.Println(bytes.Contains(chain.Transactions[len(chain.Transactions)-1].Hash.Bytes(), []byte{'\f'}))
 		readWriter.Write(append(chain.Transactions[len(chain.Transactions)-1].Hash.Bytes(), '\f')) // Write tx hash
 	} else { // No txs
 		fmt.Println(hex.EncodeToString(common.NewHash(crypto.Sha3(nil)).Bytes()))
+		fmt.Println(bytes.Contains(common.NewHash(crypto.Sha3(nil)).Bytes(), []byte{'\f'}))
 		readWriter.Write(append(common.NewHash(crypto.Sha3(nil)).Bytes(), '\f')) // Write nil hash
 	}
 
