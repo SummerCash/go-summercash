c29f4e49fa463571488186331220f7ae799f42fd
Dowland Aiello <mitsukomegumii@gmail.com>
2019-04-07 01:53:03 -0400

    p2p: Removed tests.

--- p2p/p2p_client_stream_handlers.go
+++ p2p/p2p_client_stream_handlers.go
@@ -4,7 +4,6 @@ package p2p
 import (
 	"bufio"
 	"bytes"
-	"fmt"
 	"strings"
 
 	"github.com/SummerCash/go-summercash/config"
@@ -125,8 +124,6 @@ func (client *Client) HandleReceiveBestTransaction(stream inet.Stream) {
 	}
 
 	if len(chain.Transactions) > 0 { // Check has txs
-		fmt.Println(bytes.Index(chain.Transactions[len(chain.Transactions)-1].Hash.Bytes(), []byte{'\r'})) // Log index
-		fmt.Println(string(chain.Transactions[len(chain.Transactions)-1].Hash.Bytes()))
 		readWriter.Write(append(chain.Transactions[len(chain.Transactions)-1].Hash.Bytes(), '\r')) // Write tx hash
 	} else { // No txs
 		readWriter.Write(append(common.NewHash(crypto.Sha3(nil)).Bytes(), '\r')) // Write nil hash
