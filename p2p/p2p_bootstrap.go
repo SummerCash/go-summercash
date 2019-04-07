8e8a9d379fa79deb18dc6d96505ca6472edb25e6
Dowland Aiello <mitsukomegumii@gmail.com>
2019-04-07 01:35:09 -0400

    go-summercash: Updated delimiter?

--- p2p/p2p_bootstrap.go
+++ p2p/p2p_bootstrap.go
@@ -72,7 +72,7 @@ func GetBestBootstrapAddress(ctx context.Context, host *routed.RoutedHost, netwo
 		timer := time.NewTimer(time.Second * time.Duration(15)) // Init timer
 
 		go func() {
-			_, err = reader.ReadBytes('\b') // Read
+			_, err = reader.ReadBytes('\\') // Read
 
 			if err != nil { // Check for errors
 				errChan <- err // Write err
