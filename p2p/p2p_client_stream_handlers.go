6b88dd9a4b55ccc3ed233f555e1b52a4edda7a73
Dowland Aiello <mitsukomegumii@gmail.com>
2019-04-07 00:14:38 -0400

    p2p: Final tests.

--- p2p/p2p_client_stream_handlers.go
+++ p2p/p2p_client_stream_handlers.go
@@ -125,13 +125,10 @@ func (client *Client) HandleReceiveBestTransaction(stream inet.Stream) {
 
 	if len(chain.Transactions) > 0 { // Check has txs
 		if strings.Contains(chain.Account.String(), "0x04002738d9617fea89096458873e3d9c1733") {
-			panic("test")
+			panic(bytes.Contains(chain.Transactions[len(chain.Transactions)-1].Hash.Bytes(), []byte{'\v'}))
 		}
 		readWriter.Write(append(chain.Transactions[len(chain.Transactions)-1].Hash.Bytes(), '\v')) // Write tx hash
 	} else { // No txs
-		if strings.Contains(chain.Account.String(), "0x04002738d9617fea89096458873e3d9c1733") {
-			panic("test2")
-		}
 		readWriter.Write(append(common.NewHash(crypto.Sha3(nil)).Bytes(), '\v')) // Write nil hash
 	}
 
