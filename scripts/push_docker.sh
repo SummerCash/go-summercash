3f0aa458fae05f86c6778522c1fd4cbf678d9cbb
Dowland Aiello <mitsukomegumii@gmail.com>
2019-04-19 00:09:05 -0400

    scripts: More docker push fixes.

--- scripts/push_docker.sh
+++ scripts/push_docker.sh
@@ -2,7 +2,9 @@
 # Note: this script was taken from the go-spacemesh repository.
 # All credits for this script, validate_lint.sh, go to the spacemeshos development team.
 
-docker build -t go-summercash:$(git rev-parse --abbrev-ref HEAD) . # Build docker image
+BRANCH=$(git rev-parse --abbrev-ref HEAD) # Get branch
+
+docker build -t go-summercash:$BRANCH . # Build docker image
 echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin
-docker tag go-summercash:$(BRANCH) summercash/go-summercash:$(git rev-parse --abbrev-ref HEAD)
-docker push summercash/go-summercash:$(git rev-parse --abbrev-ref HEAD)
+docker tag go-summercash:$BRANCH summercash/go-summercash:$BRANCH
+docker push summercash/go-summercash:$BRANCH
