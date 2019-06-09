d34cea26787fad7a9bcb114a5acb7d3564f23353
Dowland Aiello <mitsukomegumii@gmail.com>
2019-06-09 08:44:06 -0400

    db: Hmmmmm...

--- db/leaf_test.go
+++ db/leaf_test.go
@@ -48,4 +48,38 @@ func TestNewLeaf(t *testing.T) {
 	}
 }
 
+// TestIsOnlyChild tests the functionality of the IsOnlyChild helper method.
+func TestIsOnlyChild(t *testing.T) {
+	privateKey, err := ecdsa.GenerateKey(elliptic.P521(), rand.Reader) // Generate private key
+
+	if err != nil { // Check for errors
+		t.Error(err) // Log found error
+		t.FailNow()  // Panic
+	}
+
+	sender, err := common.NewAddress(privateKey) // Initialize address from private key
+
+	if err != nil { // Check for errors
+		t.Error(err) // Log found error
+		t.FailNow()  // Panic
+	}
+
+	transaction, err := types.NewTransaction(0, nil, &sender, &sender, big.NewFloat(0), []byte("test")) // Initialize transaction
+
+	if err != nil { // Check for errors
+		t.Error(err) // Log found error
+		t.FailNow()  // Panic
+	}
+
+	leaf, err := NewLeaf(transaction) // Initialize leaf
+
+	if err != nil { // Check for errors
+		t.Fatal(err) // Panic
+	}
+
+	isOnlyChild := leaf.IsOnlyChild() { // Check is only child
+		t.Fatal("should not be only child") // Panic
+	}
+}
+
 /* END EXPORTED METHODS HELPERS */
