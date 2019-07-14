147ab7f4a927fe8afe338e32e38f48fb2417170b
Dowland Aiello <mitsukomegumii@gmail.com>
2019-07-14 16:02:10 -0400

    go-summercash: Deleted everything lul.

--- intrnl/rpc/compileProto.sh
+++
@@ -1,8 +0,0 @@
-read -p "relative package directory (example: '../example'): "  directory # Get dir
-read -p "proto file name (example: 'handler'): " name # Get file name
-
-mkdir $name # Init support folder
-
-cd proto && mkdir $name && cd $name # Cd into new folder
-
-protoc --proto_path=$GOPATH/src:../../$directory --twirp_out=. --go_out=. ../../$directory/$name.proto # Compile proto file
