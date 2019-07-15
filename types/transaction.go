a6189ca0fc41ebd47b2db867a7fe3feecb59a6ea
Dowland Aiello <mitsukomegumii@gmail.com>
2019-07-15 01:13:19 -0400

    types: Create transaction_database.go.

--- types/transaction.go
+++ types/transaction.go
@@ -1,8 +1,36 @@
+// Package types implements many of the core go-summercash types.
 package types
+
+import (
+	"github.com/SummerCash/go-summercash/crypto"
+	"math/big"
+)
 
 // Transaction defines a generic transaction.
 type Transaction struct {
 	Nonce uint64 `json:"nonce"` // Index of transaction in account tx set
 
 	GasLimit uint64 `json:"gas_limit"` // Number of finks willing to spend on gas
+
+	Amount *big.Int `json:"amount"` // Number of finks transaction is worth (1 SMC = 1,000,000,000,000,000,000 finks)
+
+	Reward *big.Int `json:"reward_amount"` // Reward amount (in finks, of course)
+
+	Payload []byte `json:"payload"` // Transaction payload
+
+	Signature *crypto.Signature `json:"signature"` // Transaction signature
+
+	Hash crypto.Hash `json:"hash"` // Transaction hash
+
+	PreviousStateHash crypto.Hash `json:"pre_state_hash"` // Hash of contract state at previous transaction
+
+	Parents []crypto.Hash `json:"parents"` // Transaction parents
 }
+
+/* BEGIN EXPORTED METHODS */
+
+// NewTransaction initializes a new transaction with a set of parameters.
+func NewTransaction(nonce uint64, gasLimit uint64, amount *big.Int, payload []byte, lastTransaction *Transaction) (*Transaction, error) {
+}
+
+/* END EXPORTED METHODS */
---
+++ types/transaction_database.go
@@ -0,0 +1,6 @@
+// Package types implements many of the core go-summercash types.
+package types
+
+// TransactionDatabase represents a dag-based transaction db.
+type TransactionDatabase struct {
+}
